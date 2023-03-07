aa1e86ce8bcfac788d978e4e929ea50350d8dc66
Yuxuan <yy340@duke.edu>
2023-03-07 15:43:17 +0000

    add readme

--- Readme.md
+++ Readme.md
@@ -1,11 +1,21 @@
 ## structure
 
+1. single thread
+
 `Write(key, value)` -> `memory table(skiplist)` -> `Immutable memory table` -> `Disk Table`
+
+2. multi thread
+thread safe
 
 ## coding
 
 using little-endian format
 
-## skiplist
+## slice
+
+## LRU Cache
+![](lrucache.webp)
+
+## LSM tree(skiplist)
 
 ![](skiplist.webp)
--- utils/skiplist.h
+++ utils/skiplist.h
@@ -1,31 +1,6 @@
-// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file. See the AUTHORS file for names of contributors.
-
 #ifndef STORAGE_LEVELDB_DB_SKIPLIST_H_
 #define STORAGE_LEVELDB_DB_SKIPLIST_H_
 
-// Thread safety
-// -------------
-//
-// Writes require external synchronization, most likely a mutex.
-// Reads require a guarantee that the SkipList will not be destroyed
-// while the read is in progress.  Apart from that, reads progress
-// without any internal locking or synchronization.
-//
-// Invariants:
-//
-// (1) Allocated nodes are never deleted until the SkipList is
-// destroyed.  This is trivially guaranteed by the code since we
-// never delete any skip list nodes.
-//
-// (2) The contents of a Node except for the next/prev pointers are
-// immutable after the Node has been linked into the SkipList.
-// Only Insert() modifies the list, and it is careful to initialize
-// a node and use release-stores to publish the nodes in one or
-// more lists.
-//
-// ... prev vs. next pointer ordering ...
 
 #include <assert.h>
 #include <stdlib.h>
